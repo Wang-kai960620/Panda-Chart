0a6426d7282a02d35d5c2e980021f88c5c5e920f
wangkai <wk.dong.up@gmail.com>
2020-07-27 22:10:59 +0800

    背景图片做优化

--- src/Components/Uploader.tsx
+++ src/Components/Uploader.tsx
@@ -48,6 +48,11 @@ const UpLoader = observer(() => {
           <p className="ant-upload-drag-icon">
             <InboxOutlined/>
           </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+            band files
+          </p>
         </StyledDragger>
 
       </Wrapper>
